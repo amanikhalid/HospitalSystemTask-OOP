21591f0a2bf25b03b8af144eeadd4752f79fdbf9
Amani Khalid <amanikhalidd@gmail.com>
2025-07-22 14:24:47 +0400

    Added to hospital

--- Program.cs
+++ Program.cs
@@ -16,6 +16,13 @@ namespace HospitalSystemTask_OOP
 
             Patient pat1 = new Patient { Id = 101, Name = "Amani", Age = 27, PhoneNumber = "92123456" };
             Patient pat2 = new Patient { Id = 102, Name = "Abdullah", Age = 34, PhoneNumber = "93334567" };
+
+            hospital.AddDoctor(doc1); //first doctor
+            hospital.AddDoctor(doc2); // second doctor
+
+            hospital.AddPatient(pat1); // first patient
+            hospital.AddPatient(pat2); // second patient
         }
+
     }
 }
